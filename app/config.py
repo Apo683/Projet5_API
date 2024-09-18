f0304955edbd21c006a2ca84eb29331192909a51
Apo683 <knightbloody@hotmail.fr>
2024-09-18 11:45:54 +0200

    modif mlflow tracking uri

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 ML_MODEL_PATH = 'runs:/app/models/svc_model_TFIDF'
 MLB_PATH = 'app/models/mlb_fit.pkl'
 VECTORIZER_PATH = 'app/models/tfidf_vectorizer.pkl'
-MLFLOW_TRACKING_URI = "app/mlruns"
+MLFLOW_TRACKING_URI = "http://127.0.0.1:5000"
 THRESHOLD = 0.3
