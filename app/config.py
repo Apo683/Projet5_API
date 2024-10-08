b00c824067ad161b79354d6c46d61bb0430f5380
Apo683 <knightbloody@hotmail.fr>
2024-10-08 18:39:14 +0200

    correction path stockage modèles dans config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-MLB_PATH = 'app/Projet5/models/mlb_fit.pkl'
-VECTORIZER_PATH = 'app/Projet5/models/tfidf_vectorizer_final.pkl'
-ML_MODEL_PATH = 'app/Projet5/models/tfidf_svc_model.pkl'
+MLB_PATH = 'app/models/mlb_fit.pkl'
+VECTORIZER_PATH = 'app/models/tfidf_vectorizer_final.pkl'
+ML_MODEL_PATH = 'app/models/tfidf_svc_model.pkl'
 THRESHOLD = 0.3
